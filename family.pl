2031f45a43464114da8cb95bf6ba8b608f4ac663
Romain Lambermont <lambermontr@gmail.com>
2023-03-24 18:02:25 +0100

    tuto finished

--- family.pl
+++ family.pl
@@ -1,7 +1,10 @@
+#parent(parent, child).
+
 parent(john, mary).
 parent(john, peter).
 parent(mary, ann).
 parent(mary, bob).
 parent(peter, jim).
+parent(michel, juliette).
 
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
